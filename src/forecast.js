8721712cf9e782828f7311de43298370f8f0aaf3
Marcyi <48217360+Marcyi@users.noreply.github.com>
2019-05-11 17:07:48 +0200

    vypis do konzole

--- src/forecast.js
+++ src/forecast.js
@@ -1,4 +1,5 @@
 import getWeatherIcon from './weather-icons';
+
 export default class Forecast {
 
     constructor(){
@@ -6,21 +7,47 @@ export default class Forecast {
     }
 
 getForecast(){
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=Brno,cz&units=metric&lang=cz&APPID=531ce8785a5bd60577099798147f159f')
+     fetch('https://api.openweathermap.org/data/2.5/forecast?q=Brno,cz&units=metric&lang=cz&APPID=531ce8785a5bd60577099798147f159f')
     .then(response =>response.json())
     .then(data=>{
         this.displayForecast(data);
         console.log(data);
+
+       
     })
 }
 
 displayForecast(data){
 
-    const tempEl = document.querySelector('#teplota');
-    const day1El = document.querySelector('#predpoved');
+    const dayEl = document.querySelector('#predpoved');
+    dayEl.textContent = getNiceHumanForecast(data.list[8].dt);
 
-    tempEl.textContent = Math.round(data.list[8].main.temp);
-    day1El.textContent = getNiceHumanForecast(data.list[8].dt);
+
+    let superIkona = getWeatherIcon(data.list[8].weather[0].id, data.list[8].weather[0].icon);
+    let ikonaEl= document.querySelector('#ikona');
+    ikonaEl.innerHTML = superIkona;
+
+
+    console.log("zitrejsi ikona " + data.list[8].weather[0].icon);
+    console.log("nejnizsi teplota bude " + Math.round(data.list[8].main.temp_min) +" °C");
+
+
+    
+    function showForecast(data){
+        const forecast = document.querySelector('#predpoved');   
+        let html = '';
+        html = html + `
+            <div class="forecast">
+                <div class="forecast__day">${data.list[8].dt}</div>
+                <div class="forecast__icon"> ${superIkona}</i>   </div>
+                <div class="forecast__temp"> ${Math.round(data.list[8].main.temp_min)}</div>
+            </div>        
+            `
+            
+        return forecast.innerHTML = html;
+    }
+    showForecast();
+
    
     function getNiceHumanForecast(unixTimestamp) {
         let datum = new Date(unixTimestamp * 1000);
@@ -50,34 +77,12 @@ displayForecast(data){
         }
 
         console.log (`${returnDay(day)} ${date}. ${month+1}.` ) 
-
+     
     } 
 
-    
-
-
 
 }
 
 }
 
-/*
-let forecastDays = [];
-const forecastEl = document.querySelector('#pocasi')
 
-function showForecast(){
-
-    let html = '';
-    forecastDays.forEach(forecastDay =>{
-        html = html + `
-        <div class="forecast">
-            <div class="forecast__day">${forecastDay.data.list[8].dt}</div>
-            <div class="forecast__icon">                </div>
-            <div class="forecast__temp"> ${forecastDay.data.list[8].main.temp}</div>
-        </div>        
-        `
-    })
-
-    forecastEl.innerHTML = html;
-}
-*/
